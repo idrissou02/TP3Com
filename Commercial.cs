399d5a198a005432a5275e1c59da688e284ce81d
idriss <159270564+idrissou02@users.noreply.github.com>
2024-03-20 15:46:29 +0100

    Add files via upload

---
+++ Commercial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCom
+{
+    internal class Commercial
+    {
+        private string nom;
+        private string prenom;
+        private int anneNaiss;
+
+        public string Nom { get => nom; set => nom=value; }
+        public string Prenom { get => prenom; set => prenom=value; }
+        public int AnneNaiss { get => anneNaiss; set => anneNaiss=value; }
+
+        public Commercial(string nomC, string prenomC, int anneNaissC)
+        {
+            this.nom=nomC;
+            this.prenom=prenomC;
+            this.anneNaiss=anneNaissC;
+        }
+        public double calculAge()
+        {
+            return 2024 - this.anneNaiss;
+        }
+
+
+}

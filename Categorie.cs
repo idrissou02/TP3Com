7d10ce60d88f8aa50f96db991e76618050c81605
idriss <159270564+idrissou02@users.noreply.github.com>
2024-03-21 12:13:29 +0100

    Add files via upload

---
+++ Categorie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCom
+{
+    internal class Categorie
+    {
+        public string codeCat;
+        public string libCat;
+
+        public string CodeCat { get => codeCat; set => codeCat=value; }
+        public string LibCat { get => libCat; set => libCat=value; }
+
+        public Categorie(string CodeCat, string LibCat)
+        {
+            this.codeCat = CodeCat;
+            this.libCat = LibCat;
+        }
+        public override string ToString()
+        {
+            return this.codeCat+", "+this.libCat;
+        }
+
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCom
+{
+    internal class Service
+    {
+        private int serv;
+        private int effectif;
+        private Commercial[] tabCom;
+
+        public int Serv { get => serv; set => serv=value; }
+
+        public Service(int Serv)
+        {
+            this.serv = Serv;
+            tabCom = new Commercial[10];
+            effectif = 0;
+        }
+    }
+}

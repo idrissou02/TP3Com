73fcb05fd552fc7d8b4017dfb6fc47b52d62bda7
idriss <159270564+idrissou02@users.noreply.github.com>
2024-03-27 13:45:36 +0100

    Add files via upload

--- Commercial.cs
+++ Commercial.cs
@@ -8,24 +8,38 @@ namespace GestionCom
 {
     internal class Commercial
     {
-        private string nom;
-        private string prenom;
-        private int anneNaiss;
+        public string nom;
+        public string prenom;
+        public int anneNaiss;
+        public Categorie cat ;
 
         public string Nom { get => nom; set => nom=value; }
         public string Prenom { get => prenom; set => prenom=value; }
         public int AnneNaiss { get => anneNaiss; set => anneNaiss=value; }
 
-        public Commercial(string nomC, string prenomC, int anneNaissC)
+        public Commercial(string nomC, string prenomC, int anneNaissC, Categorie catC)
         {
             this.nom=nomC;
             this.prenom=prenomC;
             this.anneNaiss=anneNaissC;
+            this.cat=catC;
         }
         public double calculAge()
         {
-            return 2024 - this.anneNaiss;
+            return DateTime.Now.Year - this.anneNaiss;
         }
 
-
+        public override string ToString()
+        {
+            return "nom : "+this.nom+"\nprenom : "+this.prenom+"\nAnnée de naissance : "+this.anneNaiss+"\nCe commercial a donc "+calculAge()+"\nCatégorie : "+this.cat+"\n";
+        }
+        public void compare(Commercial c)
+        {
+            if (c.Nom == this.nom)
+            {
+                Console.WriteLine("\n Les deux commerciaux ont le même nom de famille.");
+            }
+            c.Nom=nom;
+        }
+    }
 }
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using GestionCom;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Categorie cat1 = new Categorie("SAL", "salariés");
+        Categorie cat2 = new Categorie("SAL", "salariés");
+
+        Commercial commercial1 = new Commercial("Souhil", "Idriss", 2005, cat1);
+        Commercial commercial2 = new Commercial("Simonin", "Cedrik", 2006, cat2);
+        Commercial commercial3 = new Commercial("Yasmine", "El harim", 2004, cat1);
+        Console.WriteLine("Voici les infos du commercial : \n"+"\n"+commercial1+"----------------------------\n"+commercial2+"----------------------------\n"+commercial3);
+        commercial1.compare(commercial2);
+
+        
+    }
+}
